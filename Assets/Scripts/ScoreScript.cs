d9aea33ba65cc0cc7208b97a87704a58190e73bd
LLucartt <60555543+LLucartt@users.noreply.github.com>
2020-03-10 14:05:15 +1300

    somemorestuff :)

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreScript : MonoBehaviour{
+
+    public float player1Score;
+    public float player2Score;
+    public TextMeshProUGUI p1s;
+    public TextMeshProUGUI p2s;
+
+    void Start(){
+      player1Score = 0;
+      player2Score = 0;
+    }
+
+
+    void Update(){
+      p1s.text =  player1Score.ToString();
+      p2s.text =  player2Score.ToString();
+    }
+}
